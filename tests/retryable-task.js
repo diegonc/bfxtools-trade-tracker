a571a89a23c85b409a7591be2a32e9abaf235654
Diego Nieto Cid <dnietoc@gmail.com>
2025-11-21 15:10:39 -0300

    tests: add tests for the retryable queue

---
+++ tests/retryable-task.js
@@ -0,0 +1,36 @@
+import { RetryableQueue } from '../queue/index.js'
+
+const queue = new RetryableQueue({ concurrency: 1 })
+
+function task(id) {
+  return async function taskfn(attempt) {
+    return new Promise((resolve) =>
+      setTimeout(resolve, 100 + Math.random() * 100)
+    ).then(() => console.log(`[${id}] attempt ${attempt} done`))
+  }
+}
+
+function fail(id, atAttempt) {
+  return async function taskfn(attempt) {
+    if (attempt === atAttempt) {
+      console.log(`[${id}] success at attempt ${attempt}`)
+      return
+    }
+
+    return new Promise((_, reject) =>
+      setTimeout(
+        () => reject(new Error(`[${id}] fail at attempt ${attempt}`)),
+        Math.random() * 200
+      )
+    )
+  }
+}
+
+/* TODO awaiting the queue task serializes all retry attempts */
+queue.addRetryableTask(task(1))
+queue.addRetryableTask(task(2))
+queue.addRetryableTask(fail(3, 4))
+queue.addRetryableTask(task(4))
+queue.addRetryableTask(task(5))
+queue.addRetryableTask(fail(6, 2))
+queue.addRetryableTask(task(7))
