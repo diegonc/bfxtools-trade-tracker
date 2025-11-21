e0de9578ec96960854fb9cece05d8fd4dc16ac00
Diego Nieto Cid <dnietoc@gmail.com>
2025-11-21 16:08:39 -0300

    tests/retryable: document the need for catching rejections

--- tests/retryable-task.js
+++ tests/retryable-task.js
@@ -26,11 +26,17 @@ function fail(id, atAttempt) {
   }
 }
 
-/* TODO awaiting the queue task serializes all retry attempts */
-queue.addRetryableTask(task(1))
-queue.addRetryableTask(task(2))
-queue.addRetryableTask(fail(3, 4))
-queue.addRetryableTask(task(4))
-queue.addRetryableTask(task(5))
-queue.addRetryableTask(fail(6, 2))
-queue.addRetryableTask(task(7))
+/* TODO awaiting each queue task serializes all retry attempts */
+/* TODO awaiting them all in any order works as expected... */
+await Promise.all([
+  queue.addRetryableTask(task(1)),
+  queue.addRetryableTask(task(2)),
+  queue.addRetryableTask(fail(3, 4)),
+  queue.addRetryableTask(task(4)),
+  queue.addRetryableTask(task(5)),
+  queue.addRetryableTask(fail(6, 2)),
+  queue.addRetryableTask(task(7)),
+  // ...still rejections must be caught to avoid the program stopping
+  queue.addRetryableTask(fail(8, 6)).catch((err) => console.error("err", err.message)),
+])
+console.log('All tests ran')
